2249f536d8e92853f1627e9e6a6f49761a26e39e
Alibek Beket <alibek1beket@gmail.com>
2023-02-17 05:35:52 +0600

    добавил redirect и reverse, добавил имена в пути к страницам для {% url %} в файлах html

--- app/to_do_list/urls.py
+++ app/to_do_list/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from to_do_list.views.to_do_list import *
 
 urlpatterns = [
-    path('', home_view),
-    path('to_do/', home_view),
-    path('to_do/add/', add_view),
-    path('to_do/<int:pk>', detail_view, name='detail_view')
+    path('', home_view, name='to_do_list'),
+    path('to_do/', home_view, name='to_do_list'),
+    path('to_do/add/', add_view, name='to_do_add'),
+    path('to_do/<int:pk>', detail_view, name='to_do_detail')
 ]
--- app/to_do_list/views/to_do_list.py
+++ app/to_do_list/views/to_do_list.py
@@ -1,24 +1,10 @@
 from django.core.handlers.wsgi import WSGIRequest
-from django.http import HttpResponseNotFound
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
-
 from to_do_list.models import ToDo
 
 
 def home_view(request: WSGIRequest):
-    if request.POST:
-        if request.POST.get('date') == "":
-            date = None
-        else:
-            date = request.POST.get('date')
-        to_do_add = {
-            'description': request.POST.get('description'),
-            'status': request.POST.get('status'),
-            'date': date,
-            'title': request.POST.get('title')
-        }
-        ToDo.objects.create(**to_do_add)
     to_do_list = ToDo.objects.all()
     context = {
         'to_do_list': to_do_list
@@ -27,7 +13,20 @@ def home_view(request: WSGIRequest):
 
 
 def add_view(request: WSGIRequest):
-    return render(request, 'add_page.html')
+    if not request.POST:
+        return render(request, 'add_page.html')
+    if request.POST.get('date') == "":
+        date = None
+    else:
+        date = request.POST.get('date')
+    to_do_add = {
+        'description': request.POST.get('description'),
+        'status': request.POST.get('status'),
+        'date': date,
+        'title': request.POST.get('title')
+    }
+    ToDo.objects.create(**to_do_add)
+    return redirect(reverse('to_do_list'))
 
 
 def detail_view(request, pk):
