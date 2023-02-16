417f0ab179400446daf372b077c33a97fc26416f
Alibek Beket <alibek1beket@gmail.com>
2023-02-17 04:54:26 +0600

    добавил страницу to_do/

--- app/to_do_list/urls.py
+++ app/to_do_list/urls.py
@@ -4,6 +4,7 @@ from to_do_list.views.to_do_list import *
 
 urlpatterns = [
     path('', home_view),
+    path('to_do/', home_view),
     path('to_do/add/', add_view),
     path('to_do/<int:pk>', detail_view, name='detail_view')
 ]
